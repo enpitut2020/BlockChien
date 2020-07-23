9c8920954314b9eafcadf45ebda0daf070f8c02d
afsdfsas <kv333777@yahoo.co.jp>
2020-07-23 15:34:39 +0900

    Update Main.sol

--- Main.sol
+++ Main.sol
@@ -2,12 +2,35 @@
 //メッセージ
 
 contract Main{
+    // 貸し借りの履歴
+    struct Record{
+        address User;
+        string Title;
+        uint256 Type; //0:返却する　1:借りる
+        uint256 Year;
+        uint256 Month;
+        uint256 Day;
+        bool isReturned; // 返却済みならtrue,履歴の表示で過去のものを表示しないため
+    }
+    Record record;
+
     mapping(address => uint256) public userBook; //ユーザ　=> 本
     mapping(string => uint256) public bookNum;
 
     constructor () public {
         bookNum["BlockChain"] = 1;
     }
+
+     function makeRecord(uint256 year,uint256 month, uint256 day, string memory title, uint Type) public {
+        record.Title = title;
+        record.Year = year;
+        record.Month = month;
+        record.Day = day;
+        record.Type = Type;
+    }
+    // function seeRecord() {
+
+    // }
     
 
     function returnBook(string memory book) public returns (bool){
